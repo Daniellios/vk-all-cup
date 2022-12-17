4aede3ef9977ecf6cac257e5cf472d98e6b628e0
Daniellios <danielo49134@gmail.com>
2022-12-17 16:50:53 +0300

    folders mail filter

--- src/app/trash/page.tsx
+++ src/app/trash/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-const Trash = () => {
-  return <div className="email__list">КОРЗИНА</div>;
+import path from "path";
+import fsPromises from "fs/promises";
+
+import type { IMailLetter } from "../components/Mail/interfaces";
+import MailLetter from "../components/Mail/MailLetter";
+
+const fetchMail = async () => {
+  const filePath = path.join(process.cwd(), "db.json");
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData = JSON.parse(jsonData.toString());
+  const importantMail = objectData.filter(
+    (letter: IMailLetter) => letter.folder === "Корзина"
+  );
+  return importantMail;
+};
+
+const Trash = async () => {
+  const mail = await fetchMail();
+  let counter = 0;
+
+  return (
+    <div className="email__list_wrapper">
+      <div className="email__list">
+        {mail.map((letter: IMailLetter, idx: number) => {
+          const letterComponent = (
+            <MailLetter key={idx} {...letter} id={counter}></MailLetter>
+          );
+          counter++;
+          return letterComponent;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Trash;
