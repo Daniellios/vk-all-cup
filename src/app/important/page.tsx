119da6aeff8eb7b2322ecd67fb3aa6a44c622ccf
Daniellios <danielo49134@gmail.com>
2022-12-19 18:18:01 +0300

    URL

--- src/app/important/page.tsx
+++ src/app/important/page.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 import type { IMailLetter } from "../components/Mail/interfaces";
 import MailLetter from "../components/Mail/MailLetter";
+const URL = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/sign/mail/important/Important.json?token=${process.env.NEXT_PUBLIC_SUPABASE_KEY}&t=2022-12-19T10%3A27%3A34.823Z`;
 
 const fetchMail = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/sign/mail/important/Important.json?token=${process.env.NEXT_PUBLIC_SUPABASE_KEY}&t=2022-12-19T10%3A27%3A34.823Z`
-  );
+  const response = await fetch(URL);
   const data = await response.json();
 
   return data;
