c53b722ee58fc263bfb6ae511433e37e05986c24
Daniellios <danielo49134@gmail.com>
2022-12-19 17:03:32 +0300

    f

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   experimental: {
     enableUndici: true,
     appDir: true,
